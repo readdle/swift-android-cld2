24e52755dedb9f95166d63ddf38c2ca86adffbc2
Yury Bychyk <ybychyk@readdle.com>
2017-10-18 18:03:40 +0300

    Add C wrapper for DetectLanguageSummary call

---
+++ internal/compact_lang_det_c.cc
@@ -0,0 +1,37 @@
+//
+//  compact_lang_det_c.h
+//  cld2
+//
+//  Created by Yury on 10/18/17.
+//  Copyright © 2017 yuryybk. All rights reserved.
+//
+
+#include "compact_lang_det.h"
+#include "compact_lang_det_c.h"
+
+char** detectLanguages(const char* buffer,
+                       int buffer_length) {
+    
+    char** result = new char*[3];
+    bool is_plain_text = false;
+    CLD2::Language langs[3];
+    int percents[3];
+    bool is_reliable = false;
+    DetectLanguageSummary(buffer,
+                          buffer_length,
+                          is_plain_text,
+                          langs,
+                          percents,
+                          NULL,
+                          &is_reliable);
+    
+    for (int i = 0; i < 3; i++) {
+        const char* langCode = LanguageCode(langs[i]);
+        result[i] = const_cast<char*>(langCode);
+    }
+    return result;
+}
+
+void releaseLanguages(char** langs) {
+    delete langs;
+}
